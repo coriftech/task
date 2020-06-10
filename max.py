78a34ee0dc44e3e0c086bde62bad2e04ab794a65
Cent <cent4funny@yahoo.com>
2020-06-10 11:13:12 +0100

    having error

---
+++ max.py
@@ -0,0 +1,14 @@
+ x=input("enter the value of x")
+ y = input("enter the value of y")
+ z = input("enter the value of z")
+ x1 = int(x)
+ y1 = int(y)
+ z1 = int(z)
+ if (x1 >y1) and (x1> z1):
+     max = x1
+     print (max)
+elif (y1 > x1) and (y1 > z1):
+    max = y1
+    print (max)
+else:
+    print (z)
